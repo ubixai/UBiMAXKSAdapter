b258f6d18c061e36b307c602964582abf8af1773
guoqiang <guoqiang.zhu@ubixai.com>
2024-09-19 23:10:22 +0800

    add UBiMAXKSAdapter 3.3.67.0.0

---
+++ UBiMAXKSAdapter.framework/Headers/UBiMAXKSAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UBiMAXKSAdapter.h
+//  UBiMAXKSAdapter
+//
+//  Created by guoqiang.zhu on 2024/6/19.
+//  Copyright © 2024 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UBiMAXKSAdapter : NSObject
+//
+//@end
+
+//! Project version number for UBiMAXKSAdapter.
+FOUNDATION_EXPORT double UBiMAXKSAdapterVersionNumber;
+
+//! Project version string for UBiMAXKSAdapter.
+FOUNDATION_EXPORT const unsigned char UBiMAXKSAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
